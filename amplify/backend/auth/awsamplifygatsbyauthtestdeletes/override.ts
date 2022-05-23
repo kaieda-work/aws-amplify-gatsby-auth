e3f9745230afa925d259d434f217af620ce91f52
Kaieda <kaieda@909c4acfdeee.ant.amazon.com>
2022-05-23 15:08:56 +0900

    rem custom

--- amplify/backend/auth/awsamplifygatsbyauthtestdeletes/override.ts
+++ amplify/backend/auth/awsamplifygatsbyauthtestdeletes/override.ts
@@ -1,16 +1,6 @@
 import { AmplifyAuthCognitoStackTemplate } from '@aws-amplify/cli-extensibility-helper';
 
 export function override(resources: AmplifyAuthCognitoStackTemplate) {
-    const myCustomAttribute = {
-        attributeDataType: 'String',
-        developerOnlyAttribute: false,
-        mutable: true,
-        name: 'my_custom_attribute',
-        required: false,
-      }
-      resources.userPool.schema = [ 
-        myCustomAttribute,
-      ],
       resources.userPoolClientWeb.generateSecret = false,
       resources.userPoolClientWeb.clientName = "HelloWorld2"
 
