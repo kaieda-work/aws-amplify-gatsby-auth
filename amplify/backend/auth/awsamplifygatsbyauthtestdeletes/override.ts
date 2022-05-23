4234204cda0a280d5fb5a037f0f08d715cb41504
Kaieda <kaieda@909c4acfdeee.ant.amazon.com>
2022-05-23 15:17:43 +0900

    remove normal setting

--- amplify/backend/auth/awsamplifygatsbyauthtestdeletes/override.ts
+++ amplify/backend/auth/awsamplifygatsbyauthtestdeletes/override.ts
@@ -1,7 +1,6 @@
 import { AmplifyAuthCognitoStackTemplate } from '@aws-amplify/cli-extensibility-helper';
 
 export function override(resources: AmplifyAuthCognitoStackTemplate) {
-      resources.userPoolClientWeb.generateSecret = false,
-      resources.userPoolClientWeb.clientName = "HelloWorld2"
+
 
 }
