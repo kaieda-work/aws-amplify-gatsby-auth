81eed2896c093ecce5956c41eb39fb45a50a17a5
Kaieda <kaieda@909c4acfdeee.ant.amazon.com>
2022-06-17 11:30:43 +0900

    change token validity unit

--- amplify/backend/auth/awsamplifygatsbyauthtestdeletes/override.ts
+++ amplify/backend/auth/awsamplifygatsbyauthtestdeletes/override.ts
@@ -3,5 +3,6 @@ import { AmplifyAuthCognitoStackTemplate } from '@aws-amplify/cli-extensibility-
 export function override(resources: AmplifyAuthCognitoStackTemplate) {
     resources.userPoolClientWeb.accessTokenValidity = 80;
     resources.userPoolClientWeb.idTokenValidity = 80;
+    resources.userPoolClientWeb.tokenValidityUnits = "hours";
 
 }
