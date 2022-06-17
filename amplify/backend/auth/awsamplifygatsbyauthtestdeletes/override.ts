394b9d3c073f2bb6169a7d6aee425b8052057536
Kaieda <kaieda@909c4acfdeee.ant.amazon.com>
2022-06-17 11:21:50 +0900

    override auth change

--- amplify/backend/auth/awsamplifygatsbyauthtestdeletes/override.ts
+++ amplify/backend/auth/awsamplifygatsbyauthtestdeletes/override.ts
@@ -1,6 +1,7 @@
 import { AmplifyAuthCognitoStackTemplate } from '@aws-amplify/cli-extensibility-helper';
 
 export function override(resources: AmplifyAuthCognitoStackTemplate) {
-
+    resources.userPoolClientWeb.accessTokenValidity = 80;
+    resources.userPoolClientWeb.idTokenValidity = 80;
 
 }
